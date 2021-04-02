a960b2067f211ec74c01ba87a79d8e1e87ff7d05
eunscho <46369547+eunscho@users.noreply.github.com>
2021-04-02 12:20:06 +0900

    Delete get_omega.R

--- R/get_omega.R
+++
@@ -1,7 +0,0 @@
-get_omega <- function(data) {
-  psych <- psych::omega(data, nfactors = 1)
-  psych.omega <- psych$omega.tot
-  Lambda4.omega <- Lambda4::omega.tot(data, factors = 1)
-  omega <- list(psych.omega = psych.omega, Lambda4.omega = Lambda4.omega)
-  return(omega)
-}
