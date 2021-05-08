8b1c44bb08576fbf7e028d625131734f9add1fa0
eunscho <46369547+eunscho@users.noreply.github.com>
2021-05-08 11:38:40 +0900

    Add files via upload

--- R/uni_cfa.R
+++ R/uni_cfa.R
@@ -2,11 +2,12 @@
 #' 
 #' @param cov observed covariances
 #' @param what e.g., "est", "std", "fit"
+#' @param nonneg_loading if TRUE, constraint loadings to nonnegative values
 #' @param taueq if TRUE, a tau-equivalent model is estimated
+#' @examples uni_cfa(Graham1)
 #' @export uni_cfa
 #' @return parameter estimates of unidimensional cfa model
-#' @examples uni_cfa(Graham1)
-uni_cfa <- function(cov, what = "est", nonneg_loading =FALSE, taueq = FALSE) {
+uni_cfa <- function(cov, what = "est", nonneg_loading = FALSE, taueq = FALSE) {
   stopifnot(requireNamespace("lavaan"))
   k <- nrow(cov)
   rownames(cov) <- character(length = k)
@@ -19,15 +20,18 @@ uni_cfa <- function(cov, what = "est", nonneg_loading =FALSE, taueq = FALSE) {
     } else { # congeneric
       model_str <- paste0(model_str, " + l", i, "*V", i)
       }
-    }
-  }
-  model_str <- paste0(model_str, " \n F ~~ 1*F", collapse = "\n")
-  for (i in 1:k) { # to prevent negative loadings or errors
-    if (nonneg_loading) {
-    model_str <- paste0(model_str, "\n l", i, "> .0000001")
-    model_str <- paste0(model_str, "\n V", i, " ~~ l", i, "*V", i, "\n l", i, "> 0.0000001")
   }
   colnames(cov) <- rownames(cov)
+  model_str <- paste0(model_str, " \n F ~~ 1*F", collapse = "\n")
+  if (!taueq) { # congeneric
+    for (i in 2:k) { # to prevent negative errors
+      model_str <- paste0(model_str, "\n l", i, "> .0000001")
+      if (nonneg_loading) { # prevent negative loadings
+        model_str <- paste0(model_str, "\n V", i, " ~~ l", i, "*V", i, "\n l", i, "> 0.0000001")
+      }
+    }
+  }  
+  print(model_str)
   fit <- lavaan::cfa(model_str, sample.cov = cov, sample.nobs = 500)
   out <- lavaan::inspect(fit, what = what)
   return(out)
