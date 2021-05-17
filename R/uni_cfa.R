c002274a374afccf8fcfac3567e80633bd9a09d8
eunscho <46369547+eunscho@users.noreply.github.com>
2021-05-17 13:13:32 +0900

    Add files via upload

--- R/uni_cfa.R
+++ R/uni_cfa.R
@@ -4,10 +4,12 @@
 #' @param what e.g., "est", "std", "fit"
 #' @param nonneg_loading if TRUE, constraint loadings to nonnegative values
 #' @param taueq if TRUE, a tau-equivalent model is estimated
+#' @param parallel if TRUE, a parallel model is estimated
 #' @examples uni_cfa(Graham1)
 #' @export uni_cfa
 #' @return parameter estimates of unidimensional cfa model
-uni_cfa <- function(cov, what = "est", nonneg_loading = FALSE, taueq = FALSE) {
+uni_cfa <- function(cov, what = "est", nonneg_loading = FALSE, taueq = FALSE, 
+                    parallel = FALSE) {
   stopifnot(requireNamespace("lavaan"))
   k <- nrow(cov)
   rownames(cov) <- character(length = k)
@@ -15,7 +17,7 @@ uni_cfa <- function(cov, what = "est", nonneg_loading = FALSE, taueq = FALSE) {
     rownames(cov)[i] <- paste0("V", i)
     if (i == 1) {
       model_str <- paste("F =~ NA*V1")
-    } else if (taueq) { # tau-equivalent
+    } else if (taueq | parallel) { # tau-equivalent or parallel
       model_str <- paste0(model_str, " + equal('F=~V1')*V", i)
     } else { # congeneric
       model_str <- paste0(model_str, " + l", i, "*V", i)
@@ -23,7 +25,11 @@ uni_cfa <- function(cov, what = "est", nonneg_loading = FALSE, taueq = FALSE) {
   }
   colnames(cov) <- rownames(cov)
   model_str <- paste0(model_str, " \n F ~~ 1*F", collapse = "\n")
-  if (!taueq) { # congeneric
+  if (parallel) {
+    for (i in 1:k) { # all errors are constained to be equal
+      model_str <- paste0(model_str, "\n V", i, " ~~ e*V", i)
+    }  
+  } else if (!taueq) { # congeneric
     for (i in 1:k) { # to prevent negative errors
       model_str <- paste0(model_str, "\n V", i, " ~~ e", i, "*V", i, "\n e", i, "> 0.0000001")
       if (i > 1 & nonneg_loading) { # prevent negative loadings
